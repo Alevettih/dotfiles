30e31e329066fd3940c48a3f7310390859c3e550
Alex Tykhonenko <alevettih@gmail.com>
2019-04-02 01:38:58 +0300

    fix path

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,7 +72,7 @@ echo -e "${RED}Install WebStorm${NC}"
 echo ""
 wget -O ~/Downloads/Webstorm.tar.gz "https://www.jetbrains.com/webstorm/download/download-thanks.html?platform=linux"
 tar xfz ~/Downloads/WebStorm.tar.gz -C /opt/
-cd /opt/WebStorm-*/bin
+cd /opt/WebStorm/bin
 bash ./webstorm.sh
 
 
