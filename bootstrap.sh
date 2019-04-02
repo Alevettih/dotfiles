723dc5d6efbacd5b4137b0bdc562be1f11a88a6d
Alex Tykhonenko <alevettih@gmail.com>
2019-04-02 21:39:24 +0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,89 +2,63 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-echo ""
 echo -e "${RED}Update system...${NC}"
-echo ""
 sudo apt-get update --yes       # Fetches the list of available updates
 sudo apt-get upgrade --yes       # Strictly upgrades the current packages
 sudo apt-get dist-upgrade --yes  # Installs updates (new ones)
 
-echo ""
 echo -e "${RED}Install system dependencies...${NC}"
-echo ""
 sudo apt-get install --yes \
   git \
   curl \
   ctags
 
-echo ""
 echo -e "${RED}Install slack...${NC}"
-echo ""
 sudo snap install slack --classic
 
-echo ""
 echo -e "${RED}Install git...${NC}"
-echo ""
 sudo apt-get install git --yes
 
-echo ""
 echo -e "${RED}Install skype...${NC}"
-echo ""
 sudo apt-get install -y apt-transport-https
 curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
 sudo apt-get update
 sudo apt-get install --yes skypeforlinux
 
-echo ""
 echo -e "${RED}Install chrome...${NC}"
-echo ""
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
 sudo apt-get install --yes google-chrome-stable
 
-echo ""
 echo -e "${RED}Install telegram...${NC}"
-echo ""
 wget -O- https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
 sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
 
-echo ""
 echo -e "${RED}Install node.js...${NC}"
-echo ""
 sudo apt-get install --yes nodejs gcc g++ make
 
-echo ""
 echo -e "${RED}Install Yarn...${NC}"
-echo ""
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get install --yes yarn
 
-echo ""
 echo -e "${RED}Create projects directory${NC}"
-echo ""
 mkdir ~/Projects
 
-echo ""
 echo -e "${RED}Install WebStorm${NC}"
-echo ""
 wget -O ~/Downloads/WebStorm.tar.gz "https://download.jetbrains.com/product?code=WS&latest&distribution=linux"
 sudo tar xvzf ~/Downloads/WebStorm.tar.gz -C /opt/
 cd /opt/WebStorm/bin
 bash ./webstorm.sh
 
 
-echo ""
 echo -e "${RED}Clone dotfiles project${NC}"
-echo ""
 cd ~/Projects
 git clone https://github.com/alevettih/dotfiles.git
 
-echo ""
 echo -e "${RED}Create symbol links${NC}"
-echo ""
 
 echo -e "${RED}Create symbolic link for .editorconfig${NC}"
 ln -s ~/Projects/dotfiles/.editorconfig ~/.editorconfig
@@ -97,20 +71,20 @@ ln -s ~/Projects/dotfiles/.ctags ~/.ctags
 echo -e "${RED}Create symbolic link for autostart applications${NC}"
 ln -s ~/Projects/dotfiles/autostart/* ~/.config/autostart/
 
-echo -e "${RED}Instal GNOME Tweak tools${NC}"
+echo -e "${RED}Install GNOME Tweak tools${NC}"
 sudo apt-get install gnome-tweak-tool --yes
 
-echo -e "${RED}Instal material theme${NC}"
+echo -e "${RED}Install material theme${NC}"
 sudo apt-get gnome-themes-extra gtk2-engines-murrine bc --yes
 git clone --depth 1 https://github.com/nana-4/materia-theme
 cd materia-theme
 sudo ./install.sh -c dark -s compact -g
 
-echo ""
+echo "${RED}Install dash to panel${NC}"
+git clone https://github.com/home-sweet-gnome/dash-to-panel.git
+
+
 echo -e "${RED}Run autoremove...${NC}"
-echo ""
 sudo apt autoremove --yes
 
-echo ""
 echo -e "${RED}Done...${NC}"
-echo ""
